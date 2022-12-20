d7bb700d9d8eb4aa4cabb1e99e03cc230b961850
Smith <111470120+Smith2067@users.noreply.github.com>
2022-12-20 13:32:22 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function home(){
+    window.location("https://www.google.com");
+    window.location.href = "https://www.google.com";
+  }
+  function vol(){
+    window.location("https://www.google.com");
+    window.location.href = "https://www.google.com";
+  }
+  function donate(){
+    window.location("https://www.google.com");
+    window.location.href = "https://www.google.com";
+  }
+  function faq(){
+    window.location("https://www.google.com");
+    window.location.href = "https://www.google.com";
+  }
